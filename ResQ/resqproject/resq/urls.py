c671a29481aef6ee275290096f2091ec68228ea4
Anit-K-T <anitkt15@gmail.com>
2024-09-28 10:07:47 +0530

    database entered

--- ResQ/resqproject/resq/urls.py
+++ ResQ/resqproject/resq/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path 
-  
+from .views import hospital_dashboard
 # importing views from views..py 
 from .views import resq
 from . import views
   
 urlpatterns = [ 
-    path('', resq), 
-    path('', views.index, name='index'),
-
+    path('', views.resq, name='resq'), 
+    path('index/', views.index, name='index'),
+    path('dashboard/', hospital_dashboard, name='hospital_dashboard'),
+    path('dashboard.html', views.dashboard, name='dashboard_html'),  # Added this line
 ]
--- ResQ/resqproject/resq/views.py
+++ ResQ/resqproject/resq/views.py
@@ -1,14 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from . import views
+from .models import Hospital, ProductSupplier
+
 
 
 def resq(request):
     template = loader.get_template("index.html")
     return HttpResponse(template.render())
-def index(request):
+def hospital_dashboard(request):
+    hospitals = Hospital.objects.all()
+    suppliers = ProductSupplier.objects.all()
     context = {
-        'title': 'Index Page',
-        'welcome_message': 'Welcome to my Django site!'
+        'hospitals': hospitals,
+        'suppliers': suppliers,
     }
-    return render(request, 'index.html', context)
+    return render(request, 'your_template_name.html', context)
+def index(request):
+    return render(request, 'index.html')  
+
+def dashboard(request):
+    hospitals = Hospital.objects.all()
+    suppliers = ProductSupplier.objects.all()
+    context = {
+        'hospitals': hospitals,
+        'suppliers': suppliers,
+    }
+    return render(request, 'dashboard.html',context)
+
+
